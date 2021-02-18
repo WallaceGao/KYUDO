1250a72f9ed3e35238fcce1bf73fa5a671a6c648
wallacegao <13730852881@163.com>
2021-02-18 16:27:39 +0900

    - add audio manager - add audio

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string _Name;
+    public AudioClip _Clip;
+
+    [Range(0.0f, 1.0f) ]
+    public float _Volume;
+    [Range(0.1f, 3.0f)]
+    public float _Pitch;
+
+    public bool _Loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class SoundManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public string[] PlayMusic;
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s._Clip;
+            s.source.volume = s._Volume;
+            s.source.pitch = s._Pitch;
+            s.source.loop = s._Loop;
+        }
+    }
+
+    private void Start()
+    {
+        foreach (var name in PlayMusic)
+        {
+            Play(name);
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound._Name == name);
+        if (s == null)
+        {
+            return;
+        }
+        s.source.Play();
+    }
+}

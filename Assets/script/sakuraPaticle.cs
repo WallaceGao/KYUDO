4c265310a13843f338ce48905021ed45bd5b2ca0
wallacegao <13730852881@163.com>
2021-02-26 06:48:27 +0900

    - add foucs system for particle

---
+++ Assets/script/sakuraPaticle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sakuraPaticle : MonoBehaviour
+{
+    public ParticleSystem _sakura;
+    public float _maxSpeed = 0.0f;
+    public float _foucsSpeed = 0.0f;
+    public bool _isFoucs = false;
+
+    private void Awake()
+    {
+        var main = _sakura.main;
+        main.simulationSpeed = _maxSpeed;
+    }
+
+    private void Update()
+    {
+        if (_isFoucs)
+        {
+            var main = _sakura.main;
+            main.simulationSpeed = _maxSpeed;
+        }
+        else
+        {
+            var main = _sakura.main;
+            main.simulationSpeed = _foucsSpeed;
+        }
+    }
+}
